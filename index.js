0e6e87dd1b93e02d888975c1c0b3de59a8f83a86
viktor <vitek230290@gmail.com>
2020-03-31 19:35:12 +0300

    feat: add keyListenKeyBoard and animation

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const Keyboard = {
 
   alphabet: {
     rus: [['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
-      ['tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'DEL'],
+      ['tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'del'],
       ['capslock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter'],
-      ['shiftleft', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', 'up', 'shift'],
-      ['ctrl', 'win', 'alt', 'space', 'alt', 'ctrl', 'left', 'down', 'right']],
+      ['shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '/', 'up', 'shift'],
+      ['ctrl', 'win', 'alt', ' ', 'alt', 'ctrl', 'left', 'down', 'right']],
 
     eng: [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
-      ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'DEL'],
+      ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'del'],
       ['capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter'],
-      ['shiftleft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'up', 'shift'],
-      ['ctrl', 'win', 'alt', 'space', 'alt', 'ctrl', 'left', 'down', 'right']],
+      ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'up', 'shift'],
+      ['ctrl', 'win', 'alt', ' ', 'alt', 'ctrl', 'left', 'down', 'right']],
+
+  },
+
+  keyCode: {
+    id: [['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+      ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'del'],
+      ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+      ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+      ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight']],
   },
 
   properties: {
     capsLock: false,
     shift: false,
     alt: false,
-    rusLang: true,
+    rusLang: false,
   },
 
   addElements() {
@@ -40,11 +49,28 @@ const Keyboard = {
       document.querySelector('.keyboard').appendChild(row)
       for (let j = 0; j < alphabet[i].length; j = 1 + j) {
         const btn = document.createElement('div')
-        btn.className = `btn btn-${i + 1}-${j + 1} ${alphabet[i][j]}`
+        btn.className = `btn btn-${i + 1}-${j + 1}}`
+        btn.id = `${this.keyCode.id[i][j]}`
         document.querySelector(`.row-${i + 1}`).append(btn)
-        btn.innerHTML = alphabet[i][j]
+        if (this.properties.capsLock) {
+          btn.innerHTML = alphabet[i][j].toUpperCase()
+        } else {
+          btn.innerHTML = alphabet[i][j]
+        }
       }
     }
+  },
+
+  animation(el) {
+    const element = document.getElementById(`${el}`)
+    element.style.backgroundColor = 'rgba(0, 0, 255,0.5)'
+    element.style.borderRadius = '12px'
+  },
+
+  defaultStyle(el) {
+    const currElement = document.getElementById(`${el}`)
+    currElement.style.backgroundColor = ''
+    currElement.style.borderRadius = ''
   },
 
   changeLanguage() {
@@ -52,7 +78,6 @@ const Keyboard = {
     const codes = ['ShiftLeft', 'AltLeft']
     document.addEventListener('keydown', (e) => {
       pressed.add(e.code)
-
       for (const code of codes) {
         if (!pressed.has(code)) {
           return
@@ -76,10 +101,56 @@ const Keyboard = {
     })
   },
 
+  printMassege(btn) {
+    const output = document.querySelector('.input')
+    const buttons = document.querySelectorAll('.btn')
+    buttons.forEach((el) => {
+      if (el.id === btn) {
+        output.value += el.innerHTML
+      }
+    })
+  },
+
+  checkPressedBtn(btn, event) {
+    const mainKeys = ['Backspace', 'del', 'Tab', 'CapsLock', 'Space', 'ShiftLeft', 'Enter', 'ControlLeft',
+      'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp']
+    const buttons = document.querySelectorAll('.btn')
+    if (mainKeys.includes(btn)) {
+      buttons.forEach((el) => {
+        if (el.id === btn) {
+          this.animation(el.id)
+        }
+      })
+    } else {
+      buttons.forEach((el) => {
+        if (el.id === btn) {
+          event.preventDefault()
+          this.animation(el.id)
+          this.printMassege(el.id)
+        }
+      })
+    }
+  },
+
+  listenRealKeyboard() {
+    window.addEventListener('keydown', (e) => {
+      const btn = e.code
+      this.checkPressedBtn(btn, e)
+    })
+    window.addEventListener('keyup', (e) => {
+      const buttons = document.querySelectorAll('.btn')
+      buttons.forEach((btn) => {
+        if (btn.id === e.code) {
+          this.defaultStyle(btn.id)
+        }
+      })
+    })
+  },
 }
 
 window.addEventListener('load', () => {
   Keyboard.addElements()
-  Keyboard.createKeys(Keyboard.alphabet.rus)
+  Keyboard.createKeys(Keyboard.alphabet.eng)
   Keyboard.changeLanguage()
+  Keyboard.listenRealKeyboard()
 })
