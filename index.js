34d8b2d5f3ed6e0509ece5f061512897d233a485
viktor <vitek230290@gmail.com>
2020-04-02 21:58:12 +0300

    feat: add click handler

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const Keyboard = {
 
   alphabet: {
-    rus: [['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
+    rus: [['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592'],
       ['tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', 'del'],
       ['capslock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'enter'],
-      ['shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'up', 'shift'],
-      ['ctrl', 'win', 'alt', ' ', 'alt', 'ctrl', 'left', 'down', 'right']],
+      ['shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#9650', 'shift'],
+      ['ctrl', 'win', 'alt', ' ', 'alt', 'ctrl', '&#9668', '&#9660', '&#9658']],
 
-    eng: [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace'],
+    eng: [['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '&#8592'],
       ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'del'],
       ['capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'enter'],
-      ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'up', 'shift'],
-      ['ctrl', 'win', 'alt', ' ', 'alt', 'ctrl', 'left', 'down', 'right']],
+      ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#9650', 'shift'],
+      ['ctrl', 'win', 'alt', ' ', 'alt', 'ctrl', '&#9668', '&#9660', '&#9658']],
 
   },
 
@@ -39,7 +39,7 @@ const Keyboard = {
     input.className = 'input'
     keyboard.className = 'keyboard'
     text.className = 'text'
-    text.innerHTML = 'Change language (ctrl + alt)'
+    text.innerHTML = `Change language (ctrl + alt), Click (win)`
     document.body.append(section)
     document.querySelector('.main').appendChild(input)
     document.querySelector('.main').appendChild(keyboard)
@@ -145,7 +145,7 @@ const Keyboard = {
 
   addShiftHandler(btn, event) {
     if (this.properties.shift === false) {
-      const specSymbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'backspace']
+      const specSymbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '&#8592']
       const backSlash = document.getElementById('Backslash')
       const bracketLeft = document.getElementById('BracketLeft')
       const bracketRight = document.getElementById('BracketRight')
@@ -173,8 +173,8 @@ const Keyboard = {
       bracketRight.innerHTML = '}'
       semicolon.innerHTML = ':'
       quote.innerHTML = '"'
-      comma.innerHTML = '<'
-      period.innerHTML = '>'
+      comma.innerHTML = '&#8249'
+      period.innerHTML = '&#8250'
       slash.innerHTML = '?'
 
     } else {
@@ -230,6 +230,55 @@ const Keyboard = {
     }
   },
 
+  addClickTabHandler() {
+    const output = document.querySelector('.input')
+    output.value += '  '
+  },
+
+  addClickBackspaceHandler() {
+    const output = document.querySelector('.input')
+    output.value = output.value.slice(0, -1)
+  },
+
+  addClickSpaceHandler() {
+    const output = document.querySelector('.input')
+    output.value += ' '
+  },
+
+  addClickKeyboardHandler() {
+    const keyboard = document.querySelector('.keyboard')
+    keyboard.addEventListener('mousedown', (e) => {
+      const btn = e.target.id
+      console.log(btn)
+      if (btn === 'CapsLock') {
+        this.addCapsLockHandler(btn, e)
+      } else if (btn === 'ShiftLeft' || btn === 'ShiftRight') {
+        this.addShiftHandler(btn, e)
+      } else if (btn === 'Backspace') {
+        e.preventDefault()
+        this.addClickBackspaceHandler()
+      } else if (btn === 'Tab') {
+        e.preventDefault()
+        this.addClickTabHandler()
+      } else if (btn === 'Space') {
+        e.preventDefault()
+        this.addClickSpaceHandler()
+      } else if (btn === 'MetaLeft') {
+        this.checkPressedBtn(btn, e)
+        this.changeLanguage()
+      }
+      this.checkPressedBtn(btn, e)
+    })
+    keyboard.addEventListener('mouseup', (e) => {
+      const buttons = document.querySelectorAll('.btn')
+      buttons.forEach((btn) => {
+        if (btn.id === e.target.id) {
+          this.defaultStyle(btn.id)
+        }
+      })
+    })
+  },
+
   listenRealKeyboard() {
     window.addEventListener('keydown', (e) => {
       const btn = e.code
@@ -241,6 +290,7 @@ const Keyboard = {
         e.preventDefault()
       } else if (btn === 'Tab') {
         e.preventDefault()
+        this.addClickTabHandler()
       } else if (e.altKey && e.ctrlKey) {
         this.checkPressedBtn(btn, e)
         this.changeLanguage()
@@ -269,4 +319,5 @@ window.addEventListener('load', () => {
     Keyboard.createKeys(Keyboard.alphabet.eng)
   }
   Keyboard.listenRealKeyboard()
+  Keyboard.addClickKeyboardHandler()
 })
